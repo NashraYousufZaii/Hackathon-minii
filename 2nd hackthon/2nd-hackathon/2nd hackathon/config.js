1f840f70797e6bab7dcc94131cd42218852cd17c
NashraYousufZaii <123868324+NashraYousufZaii@users.noreply.github.com>
2023-08-27 20:21:44 +0500

    Add files via upload

---
+++ 2nd hackthon/2nd-hackathon/2nd hackathon/config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBoyIVCoNgERIz-As-TPacdg4wzMFs4z5E",
+    authDomain: "nd-hackathon.firebaseapp.com",
+    projectId: "nd-hackathon",
+    storageBucket: "nd-hackathon.appspot.com",
+    messagingSenderId: "356844152264",
+    appId: "1:356844152264:web:701cf063a71c95ff854cfd",
+    measurementId: "G-QJV58LJNQ5"
+  };
